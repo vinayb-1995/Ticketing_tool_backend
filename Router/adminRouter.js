7c8b1e791944b3e440fa7c546b4b517ba7aaf297
vinayb-1995 <vinay.b@aatraltechnologies.com>
2024-11-12 09:58:15 +0530

    adminController.js,adminRouter.js file made changes

--- Router/adminRouter.js
+++ Router/adminRouter.js
@@ -21,6 +21,18 @@ router.route("/allcustomersdata").get(authMiddleware, adminController.customersC
 router.route("/allagentsdata").get(authMiddleware, adminController.agentsCollection);
 router.route("/agent/:id").get(authMiddleware, adminController.agentByID);
 
+//two update agets it
+router.route("/updateAgentByIdIt/:id").put(authMiddleware, adminController.updateAgentByIDIT);
+//two update agent sap
+router.route("/updateAgentByIdSap/:id").put(authMiddleware, adminController.updateAgentByIDSAP);
+
+//to update agentAdmin IT 
+router.route("/updateAdminAgentByIdIt").patch(authMiddleware, adminController.updateAgentAdminByIdIT);
+router.route("/updateAdminAgentByIdSap").patch(authMiddleware, adminController.updateAgentAdminByIdSap);
+
+//to update agentAdmin sap  
+
+
 //get all tickets
 router.route("/allTickets").get(authMiddleware, adminController.getAllTickets);
 
